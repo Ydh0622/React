3fbbf0b151eff113339b30f27037461df55f27f4
유덕현 <50919439+Ydh0622@users.noreply.github.com>
2026-01-01 20:37:54 +0900

    Add files via upload

---
+++ VITE_Router/src/pages/Articles.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { Link, Outlet, NavLink } from "react-router-dom";
+
+const Articles = () => {
+  // const activeStyle = {
+  //   color: "green",
+  //   fontSize: 21,
+  // };
+  return (
+    <div>
+      <Outlet />
+      <ul>
+        <ArticleItem id={1} />
+        <ArticleItem id={2} />
+        <ArticleItem id={3} />
+        {/* <li>
+          <NavLink
+            to="/articles/1"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            게시글 1
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/articles/2"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            게시글 2
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/articles/3"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            게시글 3
+          </NavLink>
+        </li> */}
+
+        {/* <li>
+          <Link to="/articles/3">게시글 3</Link>
+        </li> */}
+      </ul>
+    </div>
+  );
+};
+
+const ArticleItem = ({ id }) => {
+  const activeStyle = {
+    color: "green",
+    fontSize: 21,
+  };
+
+  return (
+    <li>
+      <NavLink
+        to={`/articles/${id}`}
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+      >
+        게시글 {id}
+      </NavLink>
+    </li>
+  );
+};
+
+export default Articles;
